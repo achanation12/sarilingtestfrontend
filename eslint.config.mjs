a4cdc256b4b6bc05aaed1b10ab47b74c4fa89844
achanation12 <galihzpcchaikall@gmail.com>
2025-04-04 13:18:13 +0700

    first commit 4

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,7 +11,10 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends({
-    extends: ['next'],
+    extends: [
+      "next/core-web-vitals",
+      "next/typescript",
+    ],
     rules: {
       'react/no-unescaped-entities': 'off',
       '@next/next/no-page-custom-font': 'off',
